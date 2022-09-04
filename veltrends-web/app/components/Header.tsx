c34e19f5efd552b8e4b775d1cefd229d8ce3acd8
graylobo <cazador7@naver.com>
2022-09-04 16:14:10 +0900

    initialize:remix project

---
+++ veltrends-web/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { colors } from "~/lib/colors";
+import { Logo } from "./vectors";
+
+export default function Header() {
+  return (
+    <Block>
+      <Logo />
+    </Block>
+  );
+}
+
+const Block = styled.header`
+  height: 56px;
+  border-bottom: 1px solid ${colors.gray0};
+  padding-left: 16px;
+  padding-right: 16px;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  svg{
+    width:84px;
+    height:17px;
+  }
+`;
